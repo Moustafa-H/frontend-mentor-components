4679828e834b3b653d1d4f6c41a9988e595de5ef
Moustafa-H <moustafa.hesham1999@gmail.com>
2023-10-18 22:08:30 +0300

    host on github pages

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,36 @@ import styles from './home.module.css'
       <h1 className={ styles.h1 }>Available routes:</h1>
       <ul className={ styles.ul }>
         <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+        <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
+
       </ul>
     </main>
   )
