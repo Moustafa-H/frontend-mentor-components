c8a260d857b4570e52d90233e61eba6ee6a91987
Moustafa-H <moustafa.hesham1999@gmail.com>
2023-10-25 11:34:33 +0300

    finished nft preview card

--- app/nft-preview-card/page.tsx
+++ app/nft-preview-card/page.tsx
@@ -3,57 +3,50 @@ import styles from './nft-preview-card.module.css'
 
 const NFTPreviewCard = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.div}>
-        <Image
-          src='./nft-preview-card/image-equilibrium.jpg'
-          alt='pew pew'
-          width={280}
-          height={280}
-          className={styles.pImage}
-          
-     />
-      <Image
-          src='./nft-preview-card/icon-view.svg'
-          alt='pew pew'
-          width={48}
-          height={48}
-          className={styles.viewImage}
-          
-     />
-      <h1 className={styles.h1}>Equilibrium #3429</h1>
-      <p className={styles.p}>Our Equilibrium collection promotes blanace and calm.</p>
-      <div className={styles.infoDiv}>
-        <div className={styles.gay}>
-            <Image
-            src='./nft-preview-card/icon-ethereum.svg'
+    <main className={ styles.main }>
+      <section className={ styles.section }>
+        <div className={ styles.pImageDiv }>
+          <Image
+            src='./nft-preview-card/image-equilibrium.jpg'
             alt='pew pew'
-            width={11}
-            height={18}
+            width={280}
+            height={280}
+            className={ styles.pImage }   
           />
-            <p className={styles.price}>0.041 ETH</p>
         </div>
-       <div className={styles.gay}>
-      <Image
-          src='./nft-preview-card/icon-clock.svg'
-          alt='pew pew'
-          width={17}
-          height={17}
-     />
-      <p className={styles.time}>3 Days Left</p>
+        <h1 className={ styles.h1 }>Equilibrium #3429</h1>
+        <p className={ styles.p }>Our Equilibrium collection promotes blanace and calm.</p>
+        <div className={ styles.infoContainer }>
+          <div className={ styles.infoDiv }>
+            <Image
+              src='./nft-preview-card/icon-ethereum.svg'
+              alt='pew pew'
+              width={11}
+              height={18}
+            />
+            <p className={ styles.price }>0.041 ETH</p>
+          </div>
+          <div className={ styles.infoDiv }>
+            <Image
+              src='./nft-preview-card/icon-clock.svg'
+              alt='pew pew'
+              width={17}
+              height={17}
+            />
+            <p className={ styles.time }>3 Days Left</p>
          </div>
-      </div>
-      <hr className={styles.line}/>
-      <div className={styles.bottomDiv}>
-      <Image
-          src='./nft-preview-card/image-avatar.png'
-          alt='pew pew'
-          width={30}
-          height={30}
-     />
-        <p className={styles.c1}>Creation of <span className={styles.ay7aga}>Jules Wyvern</span></p>
-      </div>
-      </div>
+        </div>
+        <hr className={ styles.hr } />
+        <div className={ styles.bottomDiv }>
+          <Image
+            src='./nft-preview-card/image-avatar.png'
+            alt='pew pew'
+            width={30}
+            height={30}
+          />
+          <p className={ styles.bottomText }>Creation of <span className={ styles.spanText }>Jules Wyvern</span></p>
+        </div>
+      </section>
     </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import styles from './home.module.css'
       <h1 className={ styles.h1 }>Available routes:</h1>
       <ul className={ styles.ul }>
         <li className={ styles.li }><Link href='/qr-code'>QR Code Component</Link></li>
+        <li className={ styles.li }><Link href='/nft-preview-card'>NFT Preview Card Component</Link></li>
         <li className={ styles.li }><Link href='/product-preview-card'>Product Preview Card Component</Link></li>
         <li className={ styles.li }><Link href='/intro-section-with-dropdown-navigation'>Intro Section With Dropdown Navigation</Link></li>
         <li className={ styles.li }><Link href='/todo-app'>Todo App</Link></li>
